8c4941b81e28e9c6f9631b76acae880737b5280e
Marcel Riera <mrier011@fiu.edu>
2019-09-03 03:34:17 -0400

    Imported portfolio website build for gh-pages

---
+++ precache-manifest.4e0de2b7326ad469ab0dec5bcf63ff7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef2d2762a7b7e9e905189eeb67c222e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef146d3bf1e1d88a4d1c",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "16bd123d7ad64d0decb6",
+    "url": "/static/css/main.573bf52a.chunk.css"
+  },
+  {
+    "revision": "ef146d3bf1e1d88a4d1c",
+    "url": "/static/js/2.ea17d7e3.chunk.js"
+  },
+  {
+    "revision": "16bd123d7ad64d0decb6",
+    "url": "/static/js/main.ff2fe90a.chunk.js"
+  },
+  {
+    "revision": "c86a7fc27808daecd622",
+    "url": "/static/js/runtime~main.ce17335c.js"
+  },
+  {
+    "revision": "6561ad4b4a368a4f79f9524756003a27",
+    "url": "/static/media/software.6561ad4b.png"
+  },
+  {
+    "revision": "d0abfe02a0af63523ab677152417d9d1",
+    "url": "/static/media/troubleshoot.d0abfe02.png"
+  },
+  {
+    "revision": "e4714808a99214208cc9e1bcee507faa",
+    "url": "/static/media/website.e4714808.png"
+  }
+]);
